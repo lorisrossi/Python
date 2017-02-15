ce475586f247d10aec0078fb2edef1efefe04982
Loris Rossi <lorisrossirn@gmail.com>
2017-02-15 09:00:00 +0100

    Update random_questions.py

--- random_questions.py
+++ random_questions.py
@@ -1,4 +1,4 @@
-# This Python file uses the following encoding: latin-1
+# python encoding: latin-1
 # How-to-use: read the string 'questions' and modify it
 
 import random, time
@@ -8,11 +8,16 @@ Each line correspond to a question.
 You can use these sentences to try this code.
 Enjoy!!""".splitlines()
 
-random.seed()
-command = 'something'
-while (command != 'n'):
-    print(random.choice(questions))
+random.shuffle(questions)
+N = len(questions)
+for i in range(N):
+    print i+1, "/", N
+    print questions[i]
     time.sleep(2) #stops for 2 seconds
-    #print ''
     command = raw_input("Continue? Type 'n' to stop.")
-    print ''
+    if command == 'n':
+        break
+    print
+
+print
+print "Congratulations!! Well done!"
